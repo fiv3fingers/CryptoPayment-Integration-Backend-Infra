0842c834c7b0dd1d956e3d15bee5b1c60e4a8c94
fiv3fingers <fiv3fingers.dev@gmail.com>
2025-01-16 20:22:36 +0100

    deposit same style as sale, /update

--- src/routes/payorder.py
+++ src/routes/payorder.py
@@ -1,5 +1,5 @@
 # routes/payorder.py
-from typing import List
+from typing import List, Union
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from src.database.dependencies import get_db, get_current_organization
@@ -7,7 +7,8 @@ from src.models.schemas.payorder import (
     CreatePaymentRequest,
     CreatePaymentResponse,
     PayOrderCreate,
-    PayOrderResponse
+    PayOrderResponse,
+    UpdatePayOrderRequest,
 )
 from src.models.database_models import Organization
 from src.services.payorder import PayOrderService
@@ -26,6 +27,18 @@ async def create_order(
     pay_order = await pay_order_service.create(org.id, req)
     return pay_order
 
+@router.post("/update", response_model=PayOrderResponse)
+async def update_order(
+    id: str,
+    req: UpdatePayOrderRequest,
+    org: Organization = Depends(get_current_organization),
+    db: Session = Depends(get_db)
+):
+    """ API Route for update a payorder """
+    pay_order_service = PayOrderService(db)
+    pay_order = await pay_order_service.update(id, req)
+    return pay_order
+
 @router.post("/pay", response_model=CreatePaymentResponse)
 async def pay_sale_order(
     req: CreatePaymentRequest,
@@ -34,7 +47,7 @@ async def pay_sale_order(
 ):
     """ API Route for pay a payorder """
     pay_order_service = PayOrderService(db)
-    pay_order = await pay_order_service.pay_sale(
+    pay_order = await pay_order_service.pay(
         payorder_id=req.id,
         in_token_address=req.token_address,
         in_chain_id=req.token_chain_id,
