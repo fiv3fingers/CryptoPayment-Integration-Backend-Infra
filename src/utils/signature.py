a28b0bbc1d7ac827369241d3b749079a3e2cb5bb
fiv3fingers <fiv3fingers.dev@gmail.com>
2025-01-20 14:06:22 +0100

    Add `validate_authorization_header` + make routes more RESTful

--- src/utils/signature.py
+++ src/utils/signature.py
@@ -3,12 +3,10 @@ import time
 
 from src.utils.types import AuthHeaderType
 
-def validate_signature(authHeader:str, secret:str):
-    header_parts = parse_header(authHeader)
-
+def validate_signature(header_parts:AuthHeaderType, secret:str):
     try:
         # Convert the timestamp to an integer
-        client_timestamp = int(header_parts.timestamp)
+        client_timestamp = int(header_parts["timestamp"])
     except ValueError:
         print("Invalid timestamp")
         return False
@@ -20,11 +18,11 @@ def validate_signature(authHeader:str, secret:str):
         return False
 
     # Reconstruct the expected signature
-    data = f"{header_parts.apikey}{secret}{client_timestamp}"
+    data = f"{header_parts["apikey"]}{secret}{client_timestamp}"
     hash_object = hashlib.sha512(data.encode())
     expected_signature = hash_object.hexdigest()
 
-    return expected_signature == header_parts.signature
+    return expected_signature == header_parts["signature"]
 
 #  apikey=5409jmg8qikli06ujhl3jjvokh,signature=2ee76043a90a1d1d10f1a6776dbd39f92a0f59b939d5736008e8332ba3e6963847877978666c3427fd85304cdf3b5b7dcebc3810495352d689ebb40c67993c59,timestamp=1737106896
 def parse_header(auth_header: str) -> AuthHeaderType:
