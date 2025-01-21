4707e9a38c2b10103f1fe60a2ab7cbde8fdebe93
magiclars <magiclars@protonmail.com>
2025-01-21 21:12:55 +0100

    Refactor API

--- src/models/schemas/payorder.py
+++ src/models/schemas/payorder.py
@@ -22,6 +22,80 @@ class PayOrderMetadata(BaseModel):
     items: Optional[List[MetadataItems]] = Field(default_factory=list)
 
 
+# CREATE PAYORDER
+class CreatePayOrderRequest(BaseModel):
+    mode: PayOrderMode                           = Field(examples=[PayOrderMode.SALE], title="PayOrder mode")
+    
+    # if destination_currency and destination_amount is set omit destination_value_usd
+    destination_currency: Optional[CurrencyBase] = Field(default=None)
+    destination_amount: Optional[float]          = Field(examples=[0.1], default=None)
+    
+    # if destination_value_usd is set omit destination_amount and destination_currency
+    destination_value_usd: Optional[float]       = Field(examples=[250], default=None)
+    
+    destination_receiving_address: Optional[str] = Field(examples=["0x311e128453EFd91a4c131761d9d535fF6E0EEF90"], default=None)
+    metadata: Optional[PayOrderMetadata]         = Field(default_factory=PayOrderMetadata)
+
+
+class CreatePayOrderResponse(BaseModel):
+    id: str = Field(examples=["cm5h7ubkp0000v450cwvq6kc7"])
+    mode: PayOrderMode = Field(examples=[PayOrderMode.DEPOSIT], title="PayOrder mode")
+    status: PayOrderStatus = Field(examples=[PayOrderStatus.PENDING], title="PayOrder status")
+
+    metadata: Optional[PayOrderMetadata] = Field(default_factory=PayOrderMetadata)
+    destination_amount: Optional[float]  = Field(examples=[0.1], default=None)
+    destination_currency: Optional[Currency]
+    destination_value_usd: Optional[float] = Field(examples=[250], default=None)
+
+# CREATE QUOTE
+
+class CreateQuoteRequest(BaseModel):
+    wallet_address: str = Field(examples=["0x311e128453EFd91a4c131761d9d535fF6E0EEF90"])
+    chain_id: ChainId = Field(examples=[ChainId.ETH])
+    # destination_ui_amount: Optional[float] = Field(examples=[3.5])
+
+class CreateQuoteResponse(BaseModel):
+    source_currencies: List[Currency]
+    # destination_currency: Optional[Currency]
+
+# CREATE PAYMENT DETAILS
+
+class PaymentDetailsRequest(BaseModel):
+    source_currency: CurrencyBase
+    # destination_amount: float
+    # destination_receiving_address: str
+    refund_address: str
+
+class PaymentDetailsResponse(BaseModel):
+    id: str 
+    mode: PayOrderMode
+    status: PayOrderStatus
+    expires_at: datetime
+
+    source_currency: Currency
+    deposit_address: str
+    refund_address: str
+
+    # Deposits Only 
+    destination_currency: Optional[Currency]
+    destination_receiving_address: Optional[str]
+
+# class PaySaleRequest(BaseModel):
+#     source_currency: CurrencyBase
+#     refund_address: str
+
+# class PaySaleResponse(BaseModel):
+#     id: str
+#     mode: PayOrderMode
+#     status: PayOrderStatus
+#     expires_at: datetime
+
+#     source_currency: Currency
+#     deposit_address: str
+#     #amount: int
+#     #ui_amount: float
+
+
 # CREATE & UPDATE SALE
 class CreateSaleRequest(BaseModel):
     metadata: Optional[PayOrderMetadata]    = Field(default_factory=PayOrderMetadata)
